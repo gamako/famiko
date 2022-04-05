2c9c4662eecd0320fb56d68382f39f35578a6946
gama <gama@sstinc.co.jp>
2022-04-05 18:16:40 +0900

    バイナリファイルをバッファに読み込む

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::open("./rom/hw.nes")?;
+    let mut buf = Vec::new();
+    let _ = file.read_to_end(&mut buf)?;
+    println!("{:?}", buf);
+    Ok(())
 }
