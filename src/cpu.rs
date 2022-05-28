275572145145b84041dff47dd06f65afe48ef6ae
gamako <gamako@gmail.com>
2022-05-28 22:52:53 +0900

    push, pop修正

--- src/cpu.rs
+++ src/cpu.rs
@@ -611,22 +611,22 @@ impl CPU {
 
             }
             Command::PHA => {
-                self.s -= 1;
                 self.bus.write(self.s as u16 + 0x0100, self.a);
+                self.s -= 1;
             },
             Command::PHP => {
-                self.s -= 1;
                 let v = self.p | P_MASK_BREAK_COMMAND;
                 self.bus.write(self.s as u16 + 0x0100, v);
+                self.s -= 1;
             },
             Command::PLP => {
-                let v = self.bus.read(self.s as u16 + 0x0100);
                 self.s += 1;
+                let v = self.bus.read(self.s as u16 + 0x0100);
                 self.p = (self.p & 0x30) | (v & 0xcf);
             },
             Command::PLA => {
-                let v = self.bus.read(self.s as u16 + 0x0100);
                 self.s += 1;
+                let v = self.bus.read(self.s as u16 + 0x0100);
                 self.a = v;
                 self.update_status_zero(v);
                 self.update_status_negative(v);
