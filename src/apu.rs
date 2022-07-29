d581be87ea1e59839fcb715c819cacb8eba68e11
gamako <gamako@gmail.com>
2022-07-29 23:51:47 +0900

    audio実装途中

--- src/apu.rs
+++ src/apu.rs
@@ -1,7 +1,7 @@
 
 use core::fmt;
 
-use pa::{Stream, NonBlocking, Output};
+use pa::{Stream, Blocking, Output, StreamAvailable, OutputStreamSettings};
 use portaudio as pa;
 
 const CHANNELS: i32 = 1;
@@ -9,7 +9,7 @@ const SAMPLE_RATE: f64 = 44_100.0;
 const FRAMES_PER_BUFFER: u32 = 64;
 
 pub struct Apu {
-    stream: Option<Stream<NonBlocking, Output<f32>>>,
+    stream: Option<Stream<Blocking<OutputStreamSettings<f32>::Flow>, Output<f32>>>,
 
     pulse1_reg1 : u8, // $4000 DDLC VVVV  Duty (D), envelope loop / length counter halt (L), constant volume (C), volume/envelope (V)
     pulse1_reg2 : u8, // $4001 EPPP NSSS  Sweep unit: enabled (E), period (P), negate (N), shift (S)
@@ -21,7 +21,12 @@ pub struct Apu {
     pulse1_start : bool,
     pulse1_timer_count : u64,
     pulse1_sequencer_counter : u64,
-    pulse1_buffer : Vec<u8>
+    pulse1_buffer : Vec<f32>,
+    pulse1_sample_output_couter : f32,
+    pulse1_state : f32,
+
+    pulse1_step : usize, // 
+
 }
 
 impl fmt::Debug for Apu {
@@ -44,7 +49,11 @@ impl Apu {
             pulse1_start : false,
             pulse1_timer_count : 0u64,
             pulse1_sequencer_counter : 0,
-            pulse1_buffer : vec![]
+            pulse1_buffer : vec![],
+            pulse1_sample_output_couter : 0f32,
+            pulse1_state : 0f32,
+
+            pulse1_step : 0usize,
         }
     }
 
@@ -62,25 +71,8 @@ impl Apu {
             pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER)?;
         // we won't output out of range samples so don't bother clipping them.
         settings.flags = pa::stream_flags::CLIP_OFF;
-    
-        // This routine will be called by the PortAudio engine when audio is needed. It may called at
-        // interrupt level on some machines so don't do anything that could mess up the system like
-        // dynamic resource allocation or IO.
-        let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
-            let mut idx = 0;
-            for _ in 0..frames {
-                buffer[idx] = left_saw;
-                left_saw += 0.01;
-                if left_saw >= 1.0 {
-                    left_saw -= 2.0;
-                }
-                idx += 1;
-            }
-            pa::Continue
-        };
-    
-        let mut stream = pa.open_non_blocking_stream(settings, callback)?;
-    
+        
+        let mut stream = pa.open_blocking_stream(settings)?;
         stream.start()?;
 
         self.stream = Some(stream);
@@ -126,8 +118,15 @@ impl Apu {
     // 戻り値はIRQが発生したことを知らせる
     // 44.1kHzで音を出力する場合は40.58クロックにごとに1サンプルを出力 (1/44.1K)/(1/1789773)=1789773/44100=40.58
     pub fn step(&mut self) -> bool {
+        let duty_array = [
+            [0,1,0,0,0,0,0,0],
+            [0,1,1,0,0,0,0,0],
+            [0,1,1,1,1,0,0,0],
+            [1,0,0,1,1,1,1,1],
+        ];
+
         if self.status_reg & 1 != 0 {
-            let reg = (self.pulse1_reg3 as u16) | ((self.pulse1_reg4 as u16 & 0x07) << 8);
+            let reg = (self.pulse1_reg3 as u64) | ((self.pulse1_reg4 as u64 & 0x07) << 8);
             let duty_type = self.pulse1_reg1 >> 6;
 
             if reg < 8 {
@@ -136,10 +135,52 @@ impl Apu {
             let reg_t = reg << 5 + 1;
             
             if self.pulse1_timer_count > 0 {
-                
-            } else {
+                self.pulse1_timer_count -= 0;
 
+                if self.pulse1_timer_count == 0 {
+                    self.pulse1_timer_count = reg_t / 2;
+
+                    self.pulse1_step = (self.pulse1_step + 1) % 2;
+                }
             }
+
+            self.pulse1_sample_output_couter += 1f32;
+            let sample_output_count = 1789773f32/44100.0;
+            if self.pulse1_sample_output_couter > sample_output_count {
+                self.pulse1_sample_output_couter - sample_output_count;
+
+                if self.pulse1_timer_count > 0 {
+                    let value = if self.pulse1_step == 0 {
+                        1f32
+                    } else {
+                        0f32
+                    };
+                    self.pulse1_buffer.push(value);
+                }
+
+                let write_len = self.pulse1_buffer.len();
+                if write_len > FRAMES_PER_BUFFER as usize {
+                    if let Some(stream) = self.stream {
+                        match stream.write_available() {
+                            Some(StreamAvailable::Frames(l)) => {
+                                let l = l as usize;
+                                if write_len > l{
+                                    let write_frame = &self.pulse1_buffer[0..l];
+                                    stream.write(l, |output|{
+                                        output.copy_from_slice(&write_frame);
+                                    });
+                                    let remain = write_len - l as usize;
+                                    
+                                }
+                            },
+                            Some(StreamAvailable::OutputUnderflowed) => { println!("OutputUnderflowed"); },
+                            Some(StreamAvailable::InputOverflowed) => { println!("InputOverflowed");},
+                            None => { println!("None"); }
+                        }
+                    }
+                }
+            }
+
 
             
         }
