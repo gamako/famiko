95ae4665c6b7d4179c40920ae66e847be4693630
gamako <gamako@gmail.com>
2022-08-28 08:40:47 +0900

    apuが原因のクラッシュを修正

--- src/apu_impl.rs
+++ src/apu_impl.rs
@@ -152,6 +152,7 @@ impl ApuImpl {
                 Ok(StreamAvailable::Frames(l)) => {
                     if l > (FRAMES_PER_BUFFER as i64) {
                         let write_len = std::cmp::min(buffer_len, l as usize);
+                        let write_len = std::cmp::min(FRAMES_PER_BUFFER as usize, write_len);
                         let r = stream.write(write_len as u32, |output|{
 
                             let mut i = 0;
