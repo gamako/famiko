7fc69fe594c206b5999f0f2b9c2b4ec92fc597ab
gamako <gamako@gmail.com>
2022-09-13 06:50:33 +0900

    log_compare整理

--- src/log_compare.rs
+++ src/log_compare.rs
@@ -1,23 +1,37 @@
 use std::io::{self, BufReader, BufRead};
 
-pub struct log_compare<R : io::Read> {
+pub struct LogCompare<R : io::Read> {
     log_file : BufReader<R>,
-
+    line_number : usize,
 }
 
-impl <R : io::Read> log_compare<R> {
-    pub fn new(expectFile : R) -> Self {
+impl <R : io::Read> LogCompare<R> {
+    pub fn new(expect_file : R) -> Self {
         Self {
-            log_file : BufReader::new(expectFile)
+            log_file : BufReader::new(expect_file),
+            line_number : 0,
         }
     }
 
     pub fn line(&mut self, line : &str) -> bool {
         let mut buf  = String::new();
-        self.log_file.read_line(&mut buf);
-        let chomped = &buf[..buf.len()-1];
-        
-        chomped.eq(line)
+        let result = self.log_file.read_line(&mut buf);
+        match result {
+            // EOF
+            Ok(0) => false,
+            Ok(_) => {
+                let chomped = &buf[..buf.len()-1];
+                chomped.eq(line)
+            }
+            Err(e) => {
+                println!("log file read error {:?}", e);
+                false
+            }
+        }
+    }
+
+    pub fn line_number(&self) -> usize {
+        self.line_number
     }
 }
 
@@ -31,7 +45,7 @@ mod log_compare_test {
 f1      A:00 X:00 Y:00 S:FD P:nvubdIzc  $8000: 78       SEI
 f1      A:00 X:00 Y:00 S:FD P:nvubdIzc  $8001: D8       CLD
 ".as_bytes();
-        let mut compare = log_compare::new(b);
+        let mut compare = LogCompare::new(b);
         assert!(compare.line("Log Start"));
         assert!(compare.line("f1      A:00 X:00 Y:00 S:FD P:nvubdIzc  $8000: 78       SEI"));
         assert!(compare.line("f1      A:00 X:00 Y:00 S:FD P:nvubdIzc  $8001: D8       CLD"));
