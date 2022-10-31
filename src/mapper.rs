0f4dec7dcb4385c3e5874877b5fc151ffe69fa00
gamako <gamako@gmail.com>
2022-10-31 22:56:38 +0900

    mapper実装

--- src/mapper.rs
+++ src/mapper.rs
@@ -1,14 +1,23 @@
 
 trait Mapper {
     fn read(&self, addr: u16) -> u8;
-    fn write(&self, addr: u16, v: u8);
+    fn write(&mut self, addr: u16, v: u8);
 }
 
-struct Mapper1 {
+fn new_mapper(n : u8, prg : Vec::<u8>) -> dyn Mapper {
+    Mapper0::new(prg)
+    // match n {
+    //     0 => Mapper0::new(prg),
+    //     3 => Mapper3::new(prg),
+    //     _ = > panic!("not impl %d", n)
+    // }
+}
+
+struct Mapper0 {
     prg : Vec::<u8>
 }
 
-impl Mapper1 {
+impl Mapper0 {
     fn new(prg : Vec::<u8>) -> Self {
         Self {
            prg: prg
@@ -20,7 +29,7 @@ impl Mapper for Mapper1 {
     fn read(&self, addr: u16) -> u8{
         self.prg[addr as usize]
     }
-    fn write(&self, addr: u16, v: u8) {
+    fn write(&mut self, addr: u16, v: u8) {
     }
 }
 
@@ -40,9 +49,10 @@ impl Mapper3 {
 
 impl Mapper for Mapper3 {
     fn read(&self, addr: u16) -> u8{
-        self.prg[addr as usize]
+        self.prg[addr as usize | self.bank]
     }
-    fn write(&self, addr: u16, v: u8) {
-        
+    fn write(&mut self, addr: u16, v: u8) {
+        //https://www.nesdev.org/wiki/INES_Mapper_003#Bank_select_($8000-$FFFF)
+        self.bank = (0x03 & (v as usize)) << 12;
     }
 }
